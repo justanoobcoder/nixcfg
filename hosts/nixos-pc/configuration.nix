3c6ede064df2e2b64a3c58f0a8b6e0b942bf9f8d
Nguyen Hong Hiep <syaorancode@gmail.com>
2026-01-05 18:12:03 +0700

    feat: enable appimage

--- hosts/nixos-pc/configuration.nix
+++ hosts/nixos-pc/configuration.nix
@@ -12,6 +12,12 @@
   boot.kernel.sysctl."kernel.sysrq" = 1;
   nixpkgs.config.allowUnfree = true;
 
+  # To run AppImage easily
+  programs.appimage = {
+    enable = true;
+    binfmt = true;
+  };
+
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   system.stateVersion = "25.11";
