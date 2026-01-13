f88a866a2ea5cb328cdca46c3a558087684a93f7
Nguyen Hong Hiep <syaorancode@gmail.com>
2026-01-13 17:00:38 +0700

    nvf: fix neo-tree

--- home/hiepnh/programs/nvf/filetree.nix
+++ home/hiepnh/programs/nvf/filetree.nix
@@ -3,13 +3,16 @@
     filetree.neo-tree = {
       enable = true;
       setupOpts = {
-        close_if_last_window = true;
-        window = {
-          width = 35;
-          mappings = {
-            "<cr>" = "open_tabnew";
+        sources = ["filesystem" "buffers" "git_status"];
+        open_files_do_not_replace_types = ["terminal" "Trouble" "trouble" "qf" "Outline"];
+        filesystem = {
+          bind_to_cwd = false;
+          follow_current_file = {
+            enabled = true;
           };
+          use_libuv_file_watcher = true;
         };
+        close_if_last_window = true;
       };
     };
   };
