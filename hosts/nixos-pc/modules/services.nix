a25dab7712c76adab1ba0e85424bcd125bd6cd4e
Nguyen Hong Hiep <syaorancode@gmail.com>
2026-01-19 10:54:43 +0700

    service: change power profile and brightness based on AC state

--- hosts/nixos-pc/modules/services.nix
+++ hosts/nixos-pc/modules/services.nix
@@ -1,3 +1,54 @@
-{
+{pkgs, ...}: {
   services.cloudflare-warp.enable = true;
+
+  systemd.services.power-profile-on-boot = {
+    description = "Set power profile and brightness based on AC state at boot";
+    wantedBy = ["multi-user.target"];
+    after = ["power-profiles-daemon.service"];
+    requires = ["power-profiles-daemon.service"];
+    serviceConfig.Type = "oneshot";
+
+    script = ''
+      AC=$(cat /sys/class/power_supply/AC/online 2>/dev/null || echo 1)
+
+      if [ "$AC" = "0" ]; then
+        ${pkgs.power-profiles-daemon}/bin/powerprofilesctl set power-saver
+        ${pkgs.brightnessctl}/bin/brightnessctl set 40%
+      else
+        ${pkgs.power-profiles-daemon}/bin/powerprofilesctl set balanced
+        ${pkgs.brightnessctl}/bin/brightnessctl set 100%
+      fi
+    '';
+  };
+
+  systemd.services.power-profile-on-ac-change = {
+    description = "Update power profile and brightness on AC change";
+    requires = ["power-profiles-daemon.service"];
+    serviceConfig.Type = "oneshot";
+
+    script = ''
+      AC=$(cat /sys/class/power_supply/ACAD/online 2>/dev/null)
+
+      if [ "$AC" = "0" ]; then
+        ${pkgs.power-profiles-daemon}/bin/powerprofilesctl set power-saver
+        ${pkgs.brightnessctl}/bin/brightnessctl set 40%
+      else
+        ${pkgs.power-profiles-daemon}/bin/powerprofilesctl set balanced
+        ${pkgs.brightnessctl}/bin/brightnessctl set 100%
+      fi
+    '';
+  };
+
+  # services.udev.extraRules = ''
+  #   SUBSYSTEM=="power_supply", ATTR{type}=="Mains", ATTR{online}=="0", \
+  #     RUN+="${pkgs.power-profiles-daemon}/bin/powerprofilesctl set power-saver"
+  #
+  #   SUBSYSTEM=="power_supply", ATTR{type}=="Mains", ATTR{online}=="1", \
+  #     RUN+="${pkgs.power-profiles-daemon}/bin/powerprofilesctl set balanced"
+  # '';
+
+  services.udev.extraRules = ''
+    SUBSYSTEM=="power_supply", ATTR{type}=="Mains", TAG+="systemd", \
+      ENV{SYSTEMD_WANTS}="power-profile-on-ac-change.service"
+  '';
 }
