17129d474d4f9ecd87b7ef59fbd317b724f70be2
Nguyen Hong Hiep <syaorancode@gmail.com>
2026-02-08 15:33:01 +0700

    update

--- hosts/nixos-pc/modules/user.nix
+++ hosts/nixos-pc/modules/user.nix
@@ -9,7 +9,7 @@
       "audio"
       "video"
       "networkmanager"
-      "libvirtd"
+      # "libvirtd"
     ];
     packages = [];
   };
--- hosts/nixos-pc/modules/virtualisation.nix
+++ hosts/nixos-pc/modules/virtualisation.nix
@@ -1,23 +1,23 @@
 {pkgs, ...}: {
-  virtualisation = {
-    podman = {
-      enable = true;
-      dockerCompat = true;
-    };
-    libvirtd = {
-      enable = true;
-      qemu.vhostUserPackages = with pkgs; [virtiofsd];
-    };
-  };
-
-  environment.systemPackages = with pkgs; [
-    distrobox
-    qemu
-    dnsmasq
-  ];
-  systemd.tmpfiles.rules = ["L+ /var/lib/qemu/firmware - - - - ${pkgs.qemu}/share/qemu/firmware"];
-
-  programs.virt-manager.enable = true;
-  services.qemuGuest.enable = true;
-  services.spice-vdagentd.enable = true; # enable copy and paste between host and guest
+  # virtualisation = {
+  #   podman = {
+  #     enable = true;
+  #     dockerCompat = true;
+  #   };
+  #   libvirtd = {
+  #     enable = true;
+  #     qemu.vhostUserPackages = with pkgs; [virtiofsd];
+  #   };
+  # };
+  #
+  # environment.systemPackages = with pkgs; [
+  #   distrobox
+  #   qemu
+  #   dnsmasq
+  # ];
+  # systemd.tmpfiles.rules = ["L+ /var/lib/qemu/firmware - - - - ${pkgs.qemu}/share/qemu/firmware"];
+  #
+  # programs.virt-manager.enable = true;
+  # services.qemuGuest.enable = true;
+  # services.spice-vdagentd.enable = true; # enable copy and paste between host and guest
 }
