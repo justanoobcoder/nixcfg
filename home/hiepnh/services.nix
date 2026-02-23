0dc5d6723affd7529a7df316eebce1b06a7acc4c
Nguyen Hong Hiep <syaorancode@gmail.com>
2026-02-23 19:35:40 +0700

    add automount usb feature

--- home/hiepnh/services.nix
+++ home/hiepnh/services.nix
@@ -1,3 +1,11 @@
-{
+{pkgs, ...}: {
   services.syncthing.enable = true;
+  services.udiskie = {
+    enable = true;
+    settings = {
+      program_options = {
+        file_manager = "${pkgs.pcmanfm-qt}/bin/pcmanfm-qt";
+      };
+    };
+  };
 }
--- hosts/nixos-pc/modules/services.nix
+++ hosts/nixos-pc/modules/services.nix
@@ -1,21 +1,24 @@
 {pkgs, ...}: {
-  services.cloudflare-warp.enable = true;
+  services = {
+    udisks2.enable = true;
+    cloudflare-warp.enable = true;
 
-  services.udev.extraRules = let
-    powerScript = pkgs.writeShellScript "power-profile-change" ''
-      AC=$(cat /sys/class/power_supply/AC*/online | head -n 1)
+    udev.extraRules = let
+      powerScript = pkgs.writeShellScript "power-profile-change" ''
+        AC=$(cat /sys/class/power_supply/AC*/online | head -n 1)
 
-      if [ "$AC" = "0" ]; then
-        ${pkgs.power-profiles-daemon}/bin/powerprofilesctl set power-saver
-        ${pkgs.brightnessctl}/bin/brightnessctl set 40%
-      else
-        ${pkgs.power-profiles-daemon}/bin/powerprofilesctl set balanced
-        ${pkgs.brightnessctl}/bin/brightnessctl set 100%
-      fi
+        if [ "$AC" = "0" ]; then
+          ${pkgs.power-profiles-daemon}/bin/powerprofilesctl set power-saver
+          ${pkgs.brightnessctl}/bin/brightnessctl set 40%
+        else
+          ${pkgs.power-profiles-daemon}/bin/powerprofilesctl set balanced
+          ${pkgs.brightnessctl}/bin/brightnessctl set 100%
+        fi
+      '';
+    in ''
+      SUBSYSTEM=="power_supply", ACTION=="change", RUN+="${powerScript}"
     '';
-  in ''
-    SUBSYSTEM=="power_supply", ACTION=="change", RUN+="${powerScript}"
-  '';
+  };
   systemd.services.trigger-udev-power = {
     description = "Trigger udev power rules at boot";
     wantedBy = ["multi-user.target"];
