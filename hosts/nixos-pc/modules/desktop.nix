4b0242ffa2a21f0d9fb7cd7440b86cd321a30634
Nguyen Hong Hiep <syaorancode@gmail.com>
2026-04-16 10:02:28 +0700

    override niri

--- hosts/nixos-pc/modules/desktop.nix
+++ hosts/nixos-pc/modules/desktop.nix
@@ -8,7 +8,27 @@
   hardware.bluetooth.enable = true;
 
   programs = {
-    niri.enable = true;
+    niri = {
+      enable = true;
+      package = let
+        newSrc = pkgs.fetchFromGitHub {
+          owner = "YaLTeR";
+          repo = "niri";
+          rev = "main";
+          hash = "sha256-D5ME/gcvzCqr2pqd8iw3Nx7v31CBdQLt5iFfF0PZKDw=";
+        };
+      in
+        pkgs.niri.overrideAttrs (old: {
+          src = newSrc;
+          cargoDeps = pkgs.rustPlatform.fetchCargoVendor {
+            src = newSrc;
+            hash = "sha256-XbKhPJ/VxcLf4J2I6dekKnUvCnmoXndvQsLx2B04ihE=";
+          };
+          postPatch = ''
+            patchShebangs resources/niri-session
+          '';
+        });
+    };
 
     # hyprland = {
     #   enable = true;
