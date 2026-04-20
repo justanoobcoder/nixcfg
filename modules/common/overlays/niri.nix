2fae0fe97a72977e8077673fd7946fe7551c2566
Nguyen Hong Hiep <syaorancode@gmail.com>
2026-04-20 08:48:56 +0700

    overlays(niri): update hashes

--- modules/common/overlays/niri.nix
+++ modules/common/overlays/niri.nix
@@ -4,15 +4,15 @@
       newSrc = prev.fetchFromGitHub {
         owner = "YaLTeR";
         repo = "niri";
-        rev = "main";
-        hash = "sha256-D5ME/gcvzCqr2pqd8iw3Nx7v31CBdQLt5iFfF0PZKDw=";
+        rev = "68bb942d2146cd2c8af69c0f16db18396b4388fe";
+        hash = "sha256-7Bbp0fDBJMDRpKfdHelMXbhY51bdCa5+Qn/+XONaOwk=";
       };
     in
       prev.niri.overrideAttrs (_: {
         src = newSrc;
         cargoDeps = prev.rustPlatform.fetchCargoVendor {
           src = newSrc;
-          hash = "sha256-XbKhPJ/VxcLf4J2I6dekKnUvCnmoXndvQsLx2B04ihE=";
+          hash = "sha256-tievZgYwlZ/zUjl/R6B3UFmFiav9tHxAujxPQjP6niU=";
         };
         postPatch = ''
           patchShebangs resources/niri-session
