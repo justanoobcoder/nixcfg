bb8a2065bf1fae3ab9f33b8a1938d7cdd1815610
Nguyen Hong Hiep <syaorancode@gmail.com>
2026-05-14 20:02:28 +0700

    change substituter and trusted public key of cachyos kernel

--- modules/common/features/cachyos-kernel.nix
+++ modules/common/features/cachyos-kernel.nix
@@ -1,12 +1,17 @@
-{inputs, ...}: {
-  flake.nixosModules.cachyosKernel = {
-    pkgs,
-    lib,
-    ...
-  }: {
-    nixpkgs.overlays = [inputs.nix-cachyos-kernel.overlays.default];
-    boot.kernelPackages = lib.mkForce pkgs.cachyosKernels.linuxPackages-cachyos-latest;
-    nix.settings.substituters = ["https://attic.xuyh0120.win/lantian"];
-    nix.settings.trusted-public-keys = ["lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="];
-  };
+{ inputs, ... }:
+{
+  flake.nixosModules.cachyosKernel =
+    {
+      pkgs,
+      lib,
+      ...
+    }:
+    {
+      nixpkgs.overlays = [ inputs.nix-cachyos-kernel.overlays.default ];
+      boot.kernelPackages = lib.mkForce pkgs.cachyosKernels.linuxPackages-cachyos-latest;
+      nix.settings.substituters = [ "https://cache.garnix.io" ];
+      nix.settings.trusted-public-keys = [
+        "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+      ];
+    };
 }
