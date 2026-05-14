0faa659ccfa32033649e4a9ff410c9c7367f1063
Nguyen Hong Hiep <syaorancode@gmail.com>
2026-05-14 07:52:22 +0700

    firewall: init

---
+++ modules/hosts/nixos-pc/features/firewall.nix
@@ -0,0 +1,9 @@
+{
+  flake.nixosModules.firewall = _: {
+    networking.firewall = {
+      enable = true;
+      allowedTCPPorts = [22 80 3000 8080 8888 53317];
+      allowedUDPPorts = [53317];
+    };
+  };
+}
