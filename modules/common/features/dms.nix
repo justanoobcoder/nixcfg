71972817c9dc4bfec3cbd93b83c09ad9cbe0cff3
Nguyen Hong Hiep <syaorancode@gmail.com>
2026-05-14 07:54:59 +0700

    dms: change package to git version

--- modules/common/features/dms.nix
+++ modules/common/features/dms.nix
@@ -1,7 +1,13 @@
 {
-  flake.nixosModules.dms = _: {
+  flake.nixosModules.dms = {
+    inputs,
+    pkgs,
+    ...
+  }: {
     programs.dms-shell = {
       enable = true;
+      package = inputs.dms.packages.${pkgs.stdenv.hostPlatform.system}.default;
+      quickshell.package = inputs.quickshell.packages.${pkgs.stdenv.hostPlatform.system}.quickshell;
     };
   };
 }
