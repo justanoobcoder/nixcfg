fd66cce0a29cf1f7a80669910f28a956b655d5ba
Nguyen Hong Hiep <syaorancode@gmail.com>
2026-05-15 12:52:24 +0700

    dms: no need for override

--- modules/common/features/dms.nix
+++ modules/common/features/dms.nix
@@ -5,31 +5,10 @@
       pkgs,
       ...
     }:
-    let
-      patchedDms = inputs.dms.packages.${pkgs.stdenv.hostPlatform.system}.default.overrideAttrs (old: {
-        postInstall = (old.postInstall or "") + ''
-          for f in \
-            $out/share/quickshell/dms/Modules/DankBar/Widgets/WorkspaceSwitcher.qml \
-            $out/share/quickshell/dms/Modules/DankBar/DankBarContent.qml; do
-
-            substituteInPlace "$f" \
-              --replace-warn \
-                'Hyprland.dispatch(`workspace ''${data.id}`)' \
-                'Hyprland.dispatch(`hl.dsp.focus({ workspace = ''${data.id} })`)' \
-              --replace-warn \
-                'Hyprland.dispatch(`workspace ''${modelData.id}`)' \
-                'Hyprland.dispatch(`hl.dsp.focus({ workspace = ''${modelData.id} })`)' \
-              --replace-warn \
-                'Hyprland.dispatch(`workspace ''${realWorkspaces[nextIndex].id}`)' \
-                'Hyprland.dispatch(`hl.dsp.focus({ workspace = ''${realWorkspaces[nextIndex].id} })`)'
-          done
-        '';
-      });
-    in
     {
       programs.dms-shell = {
         enable = true;
-        package = patchedDms;
+        package = inputs.dms.packages.${pkgs.stdenv.hostPlatform.system}.default;
         quickshell.package = inputs.quickshell.packages.${pkgs.stdenv.hostPlatform.system}.quickshell;
       };
     };
