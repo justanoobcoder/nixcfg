7155d885df2b621ceb367010788893209cbaeeb1
Nguyen Hong Hiep <syaorancode@gmail.com>
2026-05-19 07:43:16 +0700

    cachyos-kernel: change to lto-x86_64-v3

--- modules/common/features/cachyos-kernel.nix
+++ modules/common/features/cachyos-kernel.nix
@@ -3,15 +3,14 @@
   flake.nixosModules.cachyosKernel =
     {
       pkgs,
-      lib,
       ...
     }:
     {
       nixpkgs.overlays = [ inputs.nix-cachyos-kernel.overlays.default ];
-      boot.kernelPackages = lib.mkForce pkgs.cachyosKernels.linuxPackages-cachyos-latest;
-      nix.settings.substituters = [ "https://cache.garnix.io" ];
+      boot.kernelPackages = pkgs.cachyosKernels.linuxPackages-cachyos-lts-lto-x86_64-v3;
+      nix.settings.substituters = [ "https://attic.xuyh0120.win/lantian" ];
       nix.settings.trusted-public-keys = [
-        "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+        "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
       ];
     };
 }
