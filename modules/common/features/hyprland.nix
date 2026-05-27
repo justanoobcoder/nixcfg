8dc490ce908d0352827cf9640d4413369e68c1fd
Nguyen Hong Hiep <syaorancode@gmail.com>
2026-05-27 08:19:52 +0700

    enable xwayland for hyprland

--- modules/common/features/hyprland.nix
+++ modules/common/features/hyprland.nix
@@ -15,9 +15,15 @@
       programs.hyprland = {
         enable = true;
         withUWSM = true;
+        xwayland.enable = true;
         # package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
         # portalPackage =
         #   inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
+      };
+
+      xdg.portal = {
+        enable = true;
+        extraPortals = with pkgs; [ xdg-desktop-portal-hyprland ];
       };
     };
 }
