82f9f26fa5b49182356bfcd85ca5b6ebf2fa5ae2
Nguyen Hong Hiep <syaorancode@gmail.com>
2026-06-07 11:58:38 +0700

    remove redundant config

--- modules/common/features/hyprland.nix
+++ modules/common/features/hyprland.nix
@@ -20,10 +20,5 @@
         # portalPackage =
         #   inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
       };
-
-      xdg.portal = {
-        enable = true;
-        extraPortals = with pkgs; [ xdg-desktop-portal-hyprland ];
-      };
     };
 }
