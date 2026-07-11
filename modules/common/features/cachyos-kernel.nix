4a1e16e3bceb4865d0d4d8b49ff04e6967f81c12
Nguyen Hong Hiep <syaorancode@gmail.com>
2026-07-11 11:50:17 +0700

    switch from -lto to -bore-lto kernel

--- modules/common/features/cachyos-kernel.nix
+++ modules/common/features/cachyos-kernel.nix
@@ -6,15 +6,13 @@
       ...
     }:
     {
-      nixpkgs.overlays = [ inputs.nix-cachyos-kernel.overlays.default ];
-      boot.kernelPackages = pkgs.cachyosKernels.linuxPackages-cachyos-latest-lto-x86_64-v3;
-      nix.settings.substituters = [ "https://cache.xinux.uz" ];
+      nixpkgs.overlays = [ inputs.nix-cachyos-kernel.overlays.pinned ];
+
+      nix.settings.substituters = [ "https://attic.xuyh0120.win/lantian" ];
       nix.settings.trusted-public-keys = [
-        "cache.xinux.uz:BXCrtqejFjWzWEB9YuGB7X2MV4ttBur1N8BkwQRdH+0="
+        "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
       ];
-      # nix.settings.substituters = [ "https://attic.xuyh0120.win/lantian" ];
-      # nix.settings.trusted-public-keys = [
-      #   "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
-      # ];
+
+      boot.kernelPackages = pkgs.cachyosKernels.linuxPackages-cachyos-bore-lto-x86_64-v3;
     };
 }
